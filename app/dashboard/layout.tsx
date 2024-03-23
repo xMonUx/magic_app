87e9806def19be4d511b184311e04cefe3d6bd51
xMonUx <infinityfor0m@gmail.com>
2024-03-23 14:14:41 +0100

    Add auth

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,14 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Dashboard',
+    default: 'Dashboard',
+  },
+  description: 'The official Dashboard',
+  metadataBase: new URL('https://magic-app-nine.vercel.app/'),
+};
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+  description: 'The official dashboard',
+  metadataBase: new URL('https://https://magic-app-nine.vercel.app/')
+};
 
 export default function RootLayout({
   children,
